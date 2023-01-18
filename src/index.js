7b65bbff1de0f55bee4975ae8283a9d2103e5cc3
EugeneFed <eugenefed91@gmail.com>
2023-01-18 19:45:14 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    const str = n.toString()[0] === '-' ? n.toString().slice(1) : n.toString(); 
+
+    for (i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+
+    return result;
 }
